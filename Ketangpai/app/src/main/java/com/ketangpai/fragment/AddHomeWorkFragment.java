11ecfdf8f3b409927fe0c31a37fd30c14b93c9f4
ghit700 <332668280@qq.com>
2016-04-13 21:38:03 +0800

    添加了日期选择器和时间选择器

--- Ketangpai/app/src/main/java/com/ketangpai/fragment/AddHomeWorkFragment.java
+++ Ketangpai/app/src/main/java/com/ketangpai/fragment/AddHomeWorkFragment.java
@@ -1,23 +1,28 @@
 package com.ketangpai.fragment;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.ketangpai.adapter.AddHomeWorkDataAdapter;
 import com.ketangpai.base.BaseFragment;
 import com.ketangpai.nan.ketangpai.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
-
 
 
 /**
  * Created by nan on 2016/3/27.
  */
-public class AddHomeWorkFragment extends BaseFragment {
+public class AddHomeWorkFragment extends BaseFragment implements View.OnClickListener {
 
     //view
     EditText etHomeworkTitle;
@@ -31,6 +36,7 @@ public class AddHomeWorkFragment extends BaseFragment {
 
     //变量
     List mDataList;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_addhomework;
@@ -47,14 +53,20 @@ public class AddHomeWorkFragment extends BaseFragment {
 
     @Override
     protected void initData() {
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH) + 1;
+        tvAddHomeworkEndDate.setText(calendar.get(Calendar.YEAR) + "/" + month + "/" + calendar.get(Calendar.DAY_OF_MONTH));
+        tvAddHomeworkEndTime.setText(calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
+
         for (int i = 0; i < 10; ++i) {
-            mAddHomeWorkDataAdapter.addItem(i,"111");
+            mAddHomeWorkDataAdapter.addItem(i, "111");
         }
     }
 
     @Override
     protected void initListener() {
-
+        tvAddHomeworkEndDate.setOnClickListener(this);
+        tvAddHomeworkEndTime.setOnClickListener(this);
     }
 
     @Override
@@ -63,15 +75,49 @@ public class AddHomeWorkFragment extends BaseFragment {
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_add_homework_endDate:
+                showDateDialog();
+                break;
+            case R.id.tv_add_homework_endTime:
+                showTimeDialog();
+                break;
+
+            default:
+                break;
+        }
+    }
+
     private void initDataList() {
-        listAddHomeworkData= (RecyclerView) view.findViewById(R.id.list_add_homework_data);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false);
+        listAddHomeworkData = (RecyclerView) view.findViewById(R.id.list_add_homework_data);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false);
         listAddHomeworkData.setLayoutManager(linearLayoutManager);
-        mDataList=new ArrayList();
-        mAddHomeWorkDataAdapter=new AddHomeWorkDataAdapter(mContext,mDataList);
+        mDataList = new ArrayList();
+        mAddHomeWorkDataAdapter = new AddHomeWorkDataAdapter(mContext, mDataList);
         listAddHomeworkData.setAdapter(mAddHomeWorkDataAdapter);
 
 
+    }
 
+    private void showTimeDialog() {
+        Calendar calendar = Calendar.getInstance();
+        new TimePickerDialog(mContext, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                tvAddHomeworkEndTime.setText(hourOfDay + ":" + minute);
+            }
+        }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show();
+    }
+
+    private void showDateDialog() {
+        Calendar calendar = Calendar.getInstance();
+        new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                tvAddHomeworkEndDate.setText(year + "/" + monthOfYear + "/" + dayOfMonth);
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
